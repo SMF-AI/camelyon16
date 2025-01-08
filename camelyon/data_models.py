4abebd5d6e89d2ea24cff603316447387a4908ad
4pygmalion <4pygmalion@naver.com>
2025-01-08 09:55:18 +0900

    for test

---
+++ camelyon/data_models.py
@@ -0,0 +1,12 @@
+class Patches:
+    pass
+
+    def save(self, write_me) -> None:
+        return
+
+
+class WSI:
+    pass
+
+    def do_tile(self, write_me) -> Patches:
+        return
---
+++ main.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def get_args() -> argparse.Namespace:
+    return
+
+
+def main() -> None:
+    return
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
